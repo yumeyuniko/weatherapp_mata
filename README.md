16251242cbbea1e672e89aca5c1fb49164409114
yumeyuniko <daigocafe@msn.com>
2022-03-26 23:33:52 +0900

    update

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+## [JSON and XML Weather API and Geolocation Developer API](https://www.weatherapi.com/)
+
+![](https://res.cloudinary.com/djnhw4aey/image/upload/v1648305040/weather_lfnhqa.png)
--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,27 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { toast } from 'react-toastify';
-
-// const useRequest = (url) => {
-//   const [data, setData] = useState({});
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     axios
-//       .get(url)
-//       .then((result) => {
-//         setLoading(false);
-//         setData(result.data);
-//       })
-//       .catch((err) => {
-//         toast.error('Error loading data, check spellings');
-//       });
-//   }, [url]);
-//   return [loading, data];
-// };
-
-// export default useRequest;
-
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
